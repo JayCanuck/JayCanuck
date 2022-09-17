e8077abaa3e07e3fcaf36836b9314f632b073045
JayCanuck <robitaille88@gmail.com>
2022-09-17 11:59:54 -0700

    Add smooth scroll behaviour

--- src/components/styles/global-style.js
+++ src/components/styles/global-style.js
@@ -2,6 +2,9 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   ${'' /* put your global css here */}
+  html {
+    scroll-behavior: smooth;
+  }
   body {
     color: ${({ theme }) => theme.colors.fontColor}
   }
